92c7fc8590c4c2e549ee37e1961737d73b9a168b
cs <bartus.csongor@gmail.com>
2015-12-04 12:19:05 +0200

    Working on the filters

--- site/components/project/works/works.js
+++ site/components/project/works/works.js
@@ -10,15 +10,38 @@ var filter = function(filtersID, itemsID) {
 
   function clickFilter(event) {
     active = this.classList.contains('list__item--active');
+    attr = this.dataset.attr;
 
     if (active) {
       this.classList.remove('list__item--active');
       this.classList.add('list__item--inactive');
+      doFilter(attr, 'remove');
     } else {
       this.classList.remove('list__item--inactive');
       this.classList.add('list__item--active');
+      doFilter(attr, 'show');
+    }
+
+    doFilter();
+  }
+
+
+  function doFilter(attr, action) {
+    console.log('filtering ...');
+
+    for (var j = 0; j < items.length; j++) {
+      if (items[j].classList.contains(attr)) {
+        console.log('found:' + j);
+
+        if (action == 'show') {
+          items[j].classList.remove('thumb--inactive');
+        } else {
+          items[j].classList.add('thumb--inactive');
+        }
+      }
     }
   }
+
 
 }
 
